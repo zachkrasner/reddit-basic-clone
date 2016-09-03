cf834632fa9c87abd7296ddc8e91618c5b224639
zachkrasner <zachkrasner@gmail.com>
2016-09-02 17:50:00 -0700

    added names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_name(user)
+    if user.name.blank?
+      user.email
+    else
+      user.name
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902211726) do
+ActiveRecord::Schema.define(version: 20160903003610) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160902211726) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
